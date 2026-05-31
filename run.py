c533ba1a7f76431f029225f126122191b3876fd9
Francesco Bruno <scobru1988@gmail.com>
2026-05-31 13:32:51 +0200

    fix healthcheck localhost to 127.0.0.1

--- run.py
+++
@@ -1,6 +0,0 @@
-import os
-with open("preflight.sh", "rb") as f:
-    content = f.read().replace(b"\r\n", b"\n")
-with open("preflight.sh", "wb") as f:
-    f.write(content)
-os.system("bash preflight.sh")
--- src/server/modules/activitypub/federation.provider.ts
+++ src/server/modules/activitypub/federation.provider.ts
@@ -31,6 +31,7 @@ export interface FederationProvider {
   // Social
   getFollowers(artistId: number): Follower[];
   addFollower(artistId: number, actorUri: string, inboxUri: string): void;
+  acceptFollower(artistId: number, actorUri: string): void;
 
   // AP Notes
   createApNote(artistId: number, noteId: string, noteType: 'post' | 'release', contentId: number, contentSlug: string, contentTitle: string): number;
